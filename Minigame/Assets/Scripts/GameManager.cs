aa08521cdb6aa7740ad0e46b5b61d4cc95aa82a4
Tayri9 <tayrifernandez@gmail.com>
2023-09-19 13:19:02 +0100

    commit 5
    puntos

--- Minigame/Assets/Scripts/GameManager.cs
+++ Minigame/Assets/Scripts/GameManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Poner")]
+    [SerializeField] Slider slider;
+
+    [Header ("NoPoner")]
+    [SerializeField] int points;
+    [SerializeField] float timer = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        points = 0;
+        slider.maxValue = timer;
+        slider.minValue = 0;
+        slider.value = slider.maxValue;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+        slider.value = timer;
+
         if (Input.GetMouseButtonDown(0))
         {            
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
@@ -22,8 +35,28 @@ public class GameManager : MonoBehaviour
                 if (hitInfo.collider.CompareTag("Food"))
                 {
                     Debug.Log("tocado");
-                }                
+                    points++;
+                    timer += 5;
+                    slider.value = timer;
+                }
+                if (hitInfo.collider.CompareTag("Bomb"))
+                {
+                    points -= 5;
+                    timer -= 10;
+                    slider.value = timer;
+                }
+                if(timer > 20)
+                {
+                    timer = 20;
+                }
+            } else
+            {
+                points -= 2;
+                timer -= 5;
+                slider.value = timer;
             }
         }
     }
 }
+
+//terminar con putons negativos o si el tiempo llega a 0
