0edb57143381ea48b1eb5ee6fbf174e5553095f4
Tayri9 <tayrifernandez@gmail.com>
2023-09-19 10:27:51 +0100

    commit2

--- Minigame/Assets/Scripts/Spawner.cs
+++ Minigame/Assets/Scripts/Spawner.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] float timer = 0;
+    [Header("Poner")]    
+    [SerializeField] float timeToSpawn;    
     [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] GameObject foodPrefab;
+
+    [Header("NoPoner")]
+    [SerializeField] float timer = 0;
+    [SerializeField] float timerToSpawn;
+    [SerializeField] Vector2 position;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerToSpawn = timeToSpawn;
     }
 
     // Update is called once per frame
@@ -18,5 +27,11 @@ public class Spawner : MonoBehaviour
     {
         timer += Time.deltaTime;
         timerText.text = ((int) timer).ToString();
+
+        if(timer >= timerToSpawn)
+        {
+            timerToSpawn = timer + timeToSpawn;
+            Instantiate(foodPrefab, position);
+        }
     }
 }
