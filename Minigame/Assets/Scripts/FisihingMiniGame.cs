36bf91a13722ce3a81a51511534e4c55e4873283
Tayri9 <tayrifernandez@gmail.com>
2023-11-06 12:49:36 +0000

    gameplay

--- Minigame/Assets/Scripts/FisihingMiniGame.cs
+++ Minigame/Assets/Scripts/FisihingMiniGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FisihingMiniGame : MonoBehaviour
@@ -21,39 +22,64 @@ public class FisihingMiniGame : MonoBehaviour
 
     [Header("Hook")]
     [SerializeField] Transform hook;
-    [SerializeField] float hookPosition;
-    [SerializeField] float hookSize = 0.1f;
-    [SerializeField] float hookPower = 5f;
-    [SerializeField] float hookProgress;
-    [SerializeField] float hookPullVelocity;
+    [SerializeField] SpriteRenderer hookSpriteRender;    
+    [SerializeField] float hookSize = 0.1f;       
     [SerializeField] float hookPullPower = 0.01f;
     [SerializeField] float hookGravityPower = 0.005f;
+
+    [Header("Hook - No tocar")]
+    [SerializeField] float hookPosition;
+    [SerializeField] float hookPullVelocity;    
+
+    [Header("ProgressBar")]
+    [SerializeField] Transform progressBarContainer;
+    [SerializeField] float hookPower = 5f;   
     [SerializeField] float hookProgressDegradationPower = 0.1f;
+    [SerializeField] float hookProgress;
+
+    [Header("No tocar")]
+    [SerializeField] bool pause = false;
+    [SerializeField] float failTimer = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Resize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pause)
+        {
+            return;
+        }
         Fish();
         Hook();
+        ProgressCheck();
     }
 
     void Hook()
     {
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("click");
             hookPullVelocity += hookPullPower * Time.deltaTime;
         }
 
         hookPullVelocity -= hookGravityPower * Time.deltaTime;
 
         hookPosition += hookPullVelocity;
+
+        if(hookPosition - hookSize/2 <= 0 && hookPullVelocity < 0)
+        {
+            hookPullVelocity = 0;
+        }
+
+        if(hookPosition + hookSize/2 >= 1 && hookPullVelocity > 0)
+        {
+            hookPullVelocity = 0;
+        }
+
         hookPosition = Mathf.Clamp(hookPosition, hookSize/2, 1 - hookSize/2);
         hook.position = Vector3.Lerp(bottomPivot.position, topPivot.position, hookPosition);
     }
@@ -70,5 +96,59 @@ public class FisihingMiniGame : MonoBehaviour
 
         fishPosition = Mathf.SmoothDamp(fishPosition, fishDestination, ref fishSpeed, smoothMotion);
         fish.position = Vector3.Lerp(bottomPivot.position, topPivot.position, fishPosition);
+    }
+
+    void ProgressCheck()
+    {
+        Vector3 ls = progressBarContainer.localScale;
+        ls.y = hookProgress;
+        progressBarContainer.localScale = ls;
+
+        float min = hookPosition - hookSize / 2;
+        float max = hookPosition + hookSize / 2;
+
+        if (min < fishPosition && fishPosition < max)
+        {
+            hookProgress += hookPower * Time.deltaTime;
+        }
+        else
+        {
+            hookProgress -= hookProgressDegradationPower * Time.deltaTime;
+            
+            failTimer -= Time.deltaTime;
+            if (failTimer < 0)
+            {
+                Lose();
+            }
+        }
+
+        if(hookProgress >= 1f)
+        {
+            Win();
+        }
+
+        hookProgress = Mathf.Clamp(hookProgress, 0f, 1f);
+    }
+
+    void Win()
+    {
+        pause = true;
+        Debug.Log("win");
+    }
+
+    void Lose()
+    {
+        pause = true;
+        Debug.Log("lose");
+    }
+
+    void Resize()
+    {
+        Bounds b = hookSpriteRender.bounds;
+        float ySize = b.size.y;
+        Vector3 ls = hook.localScale;
+        float distance = Vector3.Distance(topPivot.position, bottomPivot.position);
+        ls.y = (distance / ySize * hookSize);
+        hook.localScale = ls;
     }
 }
